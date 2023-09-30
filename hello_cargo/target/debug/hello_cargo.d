4c5e0055be70ab382bb9ccf0f83d8e040f36b311
Amir Camper <51269770+HappyCamper1v1@users.noreply.github.com>
2023-09-30 09:39:42 -0500

    Everything about Cargo
    This piece of code goes over the Cargo functionality in rust

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\happy\OneDrive\Documents\GitHub\happy_learns_rust\hello_cargo\target\debug\hello_cargo.exe: C:\Users\happy\OneDrive\Documents\GitHub\happy_learns_rust\hello_cargo\src\main.rs
